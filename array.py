225fec154fef4ee067cf9299bfff42e207ff0138
qirui0327 <148060847+qirui0327@users.noreply.github.com>
2023-10-21 15:34:21 +0800

    draw three square

--- array.py
+++ array.py
@@ -2,13 +2,19 @@ import matplotlib.pyplot as plt
 import numpy as np
  
 arr= np.full((512 ,512,100),-1000)
-start_point = [200,300,400]
+start_point = [250,180,420]
 square_value = [-500,0,100]
-for i in range(len(start_point)):
-    print(i)
-    start_row,end_row = start_point[i] - 15,start_point[i] +15
-    start_col,end_col = start_point[i] - 15,start_point[i] +15
-    arr[start_row:end_row,start_col:end_col] =square_value[i]
+start_row,end_row = start_point[0] - 15,start_point[0] +15
+start_col,end_col = 250 - 15,250 +15
+arr[start_row:end_row,start_col:end_col] =square_value[0]
+
+start_row,end_row = start_point[1] - 10,start_point[1] +10
+start_col,end_col = 250 - 10,250 +10
+arr[start_row:end_row,start_col:end_col] =square_value[1]
+
+start_row,end_row = start_point[2] - 15,start_point[2] +15
+start_col,end_col = 250 - 30,250 +30
+arr[start_row:end_row,start_col:end_col] =square_value[2]
 
 for index in range (arr.shape[2]):
     plt.imshow(arr[:,:,index],cmap='grey')
