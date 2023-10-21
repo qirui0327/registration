65a4848bd463bcaeaea8a6f44006cc533b763e43
qirui0327 <qirui_ma@outlook.com>
2023-10-21 16:23:17 +0800

    generate diagram by index

--- array.py
+++ array.py
@@ -1,23 +1,37 @@
 import matplotlib.pyplot as plt
 import numpy as np
  
-arr= np.full((512 ,512,100),-1000)
-start_point = [250,180,420]
-square_value = [-500,0,100]
-start_row,end_row = start_point[0] - 15,start_point[0] +15
-start_col,end_col = 250 - 15,250 +15
-arr[start_row:end_row,start_col:end_col] =square_value[0]
 
-start_row,end_row = start_point[1] - 10,start_point[1] +10
-start_col,end_col = 250 - 10,250 +10
-arr[start_row:end_row,start_col:end_col] =square_value[1]
+# draw cricle
+def draw_circle(matrix, center_row, center_col, radius, value):
+    for i in range(matrix.shape[0]):
+        for j in range(matrix.shape[1]):
+            if (i - center_row)**2 + (j - center_col)**2 <= radius**2:
+                matrix[i][j] = value
+# draw sqaure
+def draw_square(matrix,start_point,center_point,half_side_length,value):
+    start_row,end_row = start_point - half_side_length,start_point +half_side_length
+    start_col,end_col = center_point - half_side_length,center_point +half_side_length
+    matrix[start_row:end_row,start_col:end_col] =value
 
-start_row,end_row = start_point[2] - 15,start_point[2] +15
-start_col,end_col = 250 - 30,250 +30
-arr[start_row:end_row,start_col:end_col] =square_value[2]
+# draw_square(arr,180,250,10,0)
+# draw_circle(arr, 250, 250, 20, -500)
+# draw_square(arr,420,250,30,100)
 
-for index in range (arr.shape[2]):
+
+for index in range (1,100):
+    arr= np.full((512 ,512,100),-1000)
+    if index == 1:
+       draw_square(arr,180,250,10,0)
+    elif  2<=index<=51:
+        draw_square(arr,180,250,10,0)
+        draw_circle(arr, 250, 250, 20, -500)
+    else:
+        draw_square(arr,180,250,10,0)
+        draw_circle(arr, 250, 250, 20, -500)
+        draw_square(arr,420,250,30,100)
     plt.imshow(arr[:,:,index],cmap='grey')
-    plt.show()
-    plt.close()
+    plt.title(f'Slice {index + 1}')
+    plt.axis('off')
+    plt.savefig(f'D://slice_{index + 1}.png')
 
